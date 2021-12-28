f9e467f6f9a512897b8bb1e9b4ac5307024c1685
Eric Vinicius <ericvnascimento10@gmail.com>
2021-12-28 20:27:08 -0300

    Add files via upload

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-import { useWindowDimensions, ViewProps } from 'react-native';
+import React, { useEffect } from "react";
+import { useWindowDimensions, ViewProps } from "react-native";
 import {
+  Extrapolate,
   interpolate,
   useAnimatedStyle,
   useSharedValue,
-  withTiming
-} from 'react-native-reanimated';
+  withTiming,
+} from "react-native-reanimated";
 
-import { AnimationContainer } from './styles';
+import { AnimationContainer } from "./styles";
 
 interface CardAnimationProps extends ViewProps {
   children: React.ReactNode;
@@ -21,25 +22,37 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const animatedStyle = useAnimatedStyle(() => {
     return {
       // TODO - setup animated style
-      opacity: cardOpacity.value,
+      opacity: interpolate(
+        cardOpacity.value,
+        [0, 1],
+        [0, 1],
+        Extrapolate.CLAMP
+      ),
       transform: [
-        { translateX: cardOffset.value }
-      ]
-    }
-  })
+        {
+          translateX: interpolate(
+            cardOffset.value,
+            [0.25 * displayWidth, 0],
+            [0.25 * displayWidth, 0],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
 
   useEffect(() => {
     /**
      * TODO - setup cardOpacity.value and cardOffset.value with
      * withTiming()
      */
-    cardOpacity.value = withTiming(1);
-    cardOffset.value = withTiming(0, { duration: 1000 });
+    cardOpacity.value = withTiming(1, { duration: 1000})
+    cardOffset.value = withTiming(0, { duration: 1000})
   }, []);
 
   return (
     <AnimationContainer {...rest} style={animatedStyle}>
       {children}
     </AnimationContainer>
-  )
+  );
 }
